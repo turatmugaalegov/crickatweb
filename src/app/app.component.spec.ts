fb8939c9b90987a8e076edc20a488af8be219f17
turatmugaalegov <129512166+turatmugaalegov@users.noreply.github.com>
2023-10-26 15:03:16 +0200

    reset

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'crickat'`, () => {
+  it(`should have as title 'crickatweb'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('crickat');
+    expect(app.title).toEqual('crickatweb');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('crickat app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('crickatweb app is running!');
   });
 });
