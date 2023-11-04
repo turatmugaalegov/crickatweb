da8d110972d154eb06f1f2760be526092ee199df
turatmugaalegov <129512166+turatmugaalegov@users.noreply.github.com>
2023-11-04 15:15:21 +0100

    genudel von ermin und mir

--- src/app/data/user-service.ts
+++ src/app/data/user-service.ts
@@ -1,25 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-const httpOptions = {
-    headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-    })
-}
-@Injectable()
-export class UserService {
-    constructor(private http: HttpClient) { }
-    
-    loginUser(postUser: Object) {
-        let endPoint = "https://dry-caverns-85169-185a985df7fb.herokuapp.com/api/v1.0/auth/login"
-        this.http.post(endPoint, postUser).subscribe(data => {
-            console.log(data);
-        });
-    }
+import { Observable } from 'rxjs';
 
-    logoutUser(postUser: Object) {
-        let endPoint = "https://dry-caverns-85169-185a985df7fb.herokuapp.com/api/v1.0/auth/logout"
-        this.http.post(endPoint, postUser).subscribe(data => {
-            console.log(data);
-        });
-    }
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private apiUrl = 'https://dry-caverns-85169-185a985df7fb.herokuapp.com/api/v1.0';
+
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string) {
+    return this.http.post<any>(this.apiUrl + '/auth/login', { username, password });
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,52 @@
-import { Component } from '@angular/core';
-import { UserService } from '../data/user-service';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../data/user-service'; // import the AuthService
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-  constructor(private userservice: UserService) {
-    logoutUser();
+export class LoginComponent implements OnInit {
+
+  myGroup: FormGroup;
+
+  username = ''; // for login
+  password = ''; // for login
+
+  constructor(
+    private userService: UserService // add the AuthService here
+  ) { 
+    this.myGroup = new FormGroup({
+      username: new FormControl(''),
+      password: new FormControl(''),
+    });
+  }
+
+  ngOnInit() { }
+
+  login(): void {
+    this.userService.login(this.username, this.password).subscribe(
+      response => {
+        // handle response, save the token, etc.
+        console.log(response);
+        localStorage.setItem('token', response.token); // save the token
+      },
+      error => {
+        // handle error
+        console.error(error);
+      }
+    );
+  }
+
+  storeDataOnDB(): void {
+    const token = localStorage.getItem('token'); // get the token
+    if (!token) {
+      alert('Not authenticated');
+      return;
+    }
+
+
+
   }
 }
