3b6c5484d8d64689bc242ef6764a9022fea451b5
turatmugaalegov <129512166+turatmugaalegov@users.noreply.github.com>
2023-11-04 17:53:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 app.use(express.static('./crickatweb'));
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'crickatweb/'}),
+    res.sendFile('index.html', {root: 'src/crickatweb/'}),
 );
 app.listen(process.env.PORT || 8080);
