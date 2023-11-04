90e9dcf4aedae577916a5350187a167c01ac9736
turatmugaalegov <129512166+turatmugaalegov@users.noreply.github.com>
2023-11-04 18:12:54 +0100

    1

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-app.use(express.static('./dist/src'));
+app.use(express.static('./dist/'));
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/src'}),
+    res.sendFile('index.html', {root: 'dist/'}),
 );
 app.listen(process.env.PORT || 8080);
