306545fa2ab410e314aedade72fe1dcc396dbf55
turatmugaalegov <129512166+turatmugaalegov@users.noreply.github.com>
2023-11-04 17:47:59 +0100

    3

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+app.use(express.static('./crickatweb'));
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'crickatweb/'}),
+);
+app.listen(process.env.PORT || 8080);
