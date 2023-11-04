2192fe7ebc7c6106de71c7c2aef092180d2ca4ef
turatmugaalegov <129512166+turatmugaalegov@users.noreply.github.com>
2023-11-04 17:45:42 +0100

    2

--- src/app/data/event-service.ts
+++ src/app/data/event-service.ts
@@ -1,35 +1,41 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
 const httpOptions = {
     headers: new HttpHeaders({
         'Content-Type': 'application/json'
     })
 }
-
-
-@Injectable({
-    providedIn: 'root'
-  })
-  export class EventService {
-    private apiUrl = 'https://dry-caverns-85169-185a985df7fb.herokuapp.com/api/v1.0'; 
-  
+@Injectable()
+export class EventService {
     constructor(private http: HttpClient) { }
-
-    getAllEvents() {
-        
+    getData() {
+        return this.http.get('https://dry-caverns-85169-185a985df7fb.herokuapp.com/api/v1.0/events/all');
     }
-  
-    login(username: string, password: string) {
-      return this.http.post<any>(`${this.apiUrl}/auth/login`, { username, password });
+    addData(postEvent: Object) {
+        let endPoint = "https://dry-caverns-85169-185a985df7fb.herokuapp.com/api/v1.0/create-event"
+        this.http.post(endPoint, postEvent).subscribe(data => {
+            console.log(data);
+        });
     }
-
-    register(username: string, name: string, email: string, password: string) {
-      return this.http.post<any>(`${this.apiUrl}/auth/register`, { username, name, email, password});
+    editData(patchEvent: Object) {
+        let endPoint = "https://dry-caverns-85169-185a985df7fb.herokuapp.com/api/v1.0/edit-event/{id}"
+        this.http.post(endPoint, patchEvent).subscribe(data => {
+            console.log(data);
+        });
     }
-
-    getUserRole() {
-      return "event_host";
+    deleteData(deleteEvent: Object) {
+        let endPoint = "https://dry-caverns-85169-185a985df7fb.herokuapp.com/api/v1.0/delete-event/{id}"
+        this.http.post(endPoint, deleteEvent).subscribe(data => {
+            console.log(data);
+        });
     }
-
+    favData(postFavoriteEvent: Object) {
+        let endPoint = "https://dry-caverns-85169-185a985df7fb.herokuapp.com/api/v1.0/favorite-event/{id}"
+        this.http.post(endPoint, postFavoriteEvent).subscribe(data => {
+            console.log(data);
+        });
+    }
+    getallfavData() {
+        return this.http.get('https://dry-caverns-85169-185a985df7fb.herokuapp.com/api/v1.0/events/favorites');
+    }
 }
