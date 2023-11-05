9b3c9d3244467d7cc2e4162a6c52f999f37d2e79
Ermin <ermin909090@live.com>
2023-11-05 15:05:50 +0100

    Update eventlist.component.ts

--- src/app/eventlist/eventlist.component.ts
+++ src/app/eventlist/eventlist.component.ts
@@ -9,9 +9,5 @@ export class EventlistComponent implements OnInit {
   public events: any;
   constructor(private eventservice: EventService) {}
   ngOnInit() {
-    this.eventservice.getData().subscribe(
-      data => { this.events = data },
-        err => console.log(err),
-        () => console.log('loading done.')
-); }
+    this.eventservice; }
 }
