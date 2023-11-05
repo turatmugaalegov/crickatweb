cd5380a2e7815eb14c9b24fcf5b14430534e4699
turatmugaalegov <129512166+turatmugaalegov@users.noreply.github.com>
2023-11-05 20:27:13 +0100

    event info works

--- src/app/eventdialog/eventdialog.component.ts
+++ src/app/eventdialog/eventdialog.component.ts
@@ -8,4 +8,20 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class EventdialogComponent {
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+
+  eventInfo: any[] = []; // Hier wird die Liste der Event-Informationen gespeichert
+
+  ngOnInit() {
+    // Erstelle die Liste der Event-Informationen
+    this.eventInfo = [
+      { label: 'ID', value: this.data.id },
+      { label: 'Name', value: this.data.name },
+      { label: 'Date', value: this.data.date },
+      { label: 'Type', value: this.data.type },
+      { label: 'Age Rating', value: this.data.ageRating },
+      { label: 'Host', value: this.data.host },
+      { label: 'Ticket Price', value: this.data.ticketPrice },
+      { label: 'Location', value: this.data.location },
+    ];
+  }
 }
