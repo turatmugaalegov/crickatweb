8c87ed1534ac5919ca3bbfdb3360075347c7da57
turatmugaalegov <129512166+turatmugaalegov@users.noreply.github.com>
2023-11-05 19:35:20 +0100

    eventlist mit sort und paginator

--- src/app/eventlist/eventlist.component.ts
+++ src/app/eventlist/eventlist.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { EventService } from '../data/event-service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatDialog } from '@angular/material/dialog';
 @Component({
   selector: 'app-eventlist',
   templateUrl: './eventlist.component.html',
@@ -8,10 +12,17 @@ import { EventService } from '../data/event-service';
 export class EventlistComponent implements OnInit {
   public events: any;
   displayedColumns: string[] = ['id', 'name', 'date', 'type', 'ageRating', 'host', 'ticketPrice', 'location'];
+
+  @ViewChild(MatPaginator) paginator !:MatPaginator;
+  @ViewChild(MatSort) sort !:MatSort;
+
   constructor(private eventservice: EventService) {}
   ngOnInit() {
     this.eventservice.getEvents().subscribe(data => {
       this.events = data;
+      this.events = new MatTableDataSource(this.events);
+      this.events.paginator = this.paginator;
+      this.events.sort = this.sort;
     }); 
   }
 }
