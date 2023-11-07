af252d0dd496164fb94fcd8927f6f5c1d9949bab
turatmugaalegov <129512166+turatmugaalegov@users.noreply.github.com>
2023-11-07 16:09:59 +0100

    home block

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserService } from '../data/user-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  isloggedinHEAD=false;
+  isnotloggedinHEAD=true;
+
+  constructor(private router:Router, private userService:UserService){
+  }
+
+  ngOnInit() {
+    if(this.userService.getLoggedStatus()){
+      this.isloggedinHEAD=true;
+      this.isnotloggedinHEAD=false;
+    } else {
+      this.isloggedinHEAD=false;
+      this.isnotloggedinHEAD=true;
+    }
+
+    if(this.isnotloggedinHEAD) {
+      this.router.navigate(['/login']);
+    }
+  }
+
+  ngDoCheck(): void {
+    
+  }
 
 }
